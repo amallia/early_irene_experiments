7160d71cfa51c6501bd316cc6fd78ff8a83f951a
John Foley <jfoley@cs.umass.edu>
2017-11-27 11:22:26 -0500

    Code to run true pooling for Test200Baseline data

--- src/main/kotlin/edu/umass/cics/ciir/irene/example/IndexTrecCarParagraphs.kt
+++ src/main/kotlin/edu/umass/cics/ciir/irene/example/IndexTrecCarParagraphs.kt
@@ -3,10 +3,13 @@ package edu.umass.cics.ciir.irene.example
 import edu.umass.cics.ciir.chai.CountingDebouncer
 import edu.umass.cics.ciir.chai.Debouncer
 import edu.umass.cics.ciir.chai.smartDoLines
+import edu.umass.cics.ciir.chai.smartPrint
 import edu.umass.cics.ciir.iltr.pagerank.SpacesRegex
 import edu.umass.cics.ciir.irene.*
 import edu.umass.cics.ciir.sprf.NamedMeasures
 import edu.umass.cics.ciir.sprf.getEvaluators
+import edu.umass.cics.ciir.sprf.inqueryStop
+import edu.umass.cics.ciir.sprf.pmake
 import edu.unh.cs.treccar.Data
 import edu.unh.cs.treccar.read_data.DeserializeData
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer
@@ -38,7 +41,7 @@ fun main(args: Array<String>) {
     val msg = Debouncer()
     // Seven million paragraphs (as described in paper)
     // much more in actual 16GB dump
-    val total = argp.get("total", 29678367L)
+    val total = argp.get("total", 29_678_367L)
 
     IreneIndexer(getTrecCarIndexParams(indexPath).apply { create() }).use { writer ->
         StreamCreator.openInputStream(paragraphsInput).use { input ->
@@ -104,42 +107,89 @@ object CountDocuments {
     }
 }
 
+// QL performance: map=0.151 r-prec=0.114 recip_rank=0.211
+// BM25 from paper: map=0.150 r-prec=0.118 recip_rank=0.216
 object Test200Baseline {
     @JvmStatic fun main(args: Array<String>) {
         val basePath = File("/mnt/scratch/jfoley/trec-car/")
         val argp = Parameters.parseArgs(args)
         val qrelsPath = File(argp.get("qrels", File(basePath, "test200/train.test200.fold0.cbor.hierarchical.qrels").absolutePath))
-        val measures = getEvaluators("map", "recip_rank", "r-prec")
+        val measures = getEvaluators("map", "recip_rank", "r-prec", "r")
         val summary = NamedMeasures()
 
         val (queries, qrels) = loadTrecCarDataset(qrelsPath)
         println(queries.size)
 
-        IreneIndex(getTrecCarIndexParams(File(argp.get("index", "/mnt/scratch/jfoley/trec-car/paragraphs.irene2")))).use { index ->
-            index.env.estimateStats = "min"
-            val msg = CountingDebouncer(queries.size.toLong())
-            queries.forEach { (qid, qtext) ->
-                val qj = qrels[qid] ?: error("No judgments for $qid")
-                val terms = index.tokenize(qtext)
-                //val qsdm = SequentialDependenceModel(terms, stopwords = inqueryStop)
-                val ql = QueryLikelihood(terms)
+        File("data/trec-car-test200.irene-sdm.qlpool.jsonl.gz").smartPrint { output ->
+            IreneIndex(getTrecCarIndexParams(File(argp.get("index", "/mnt/scratch/jfoley/trec-car/paragraphs.irene2")))).use { index ->
+                index.env.estimateStats = "min"
+                val msg = CountingDebouncer(queries.size.toLong())
+                queries.entries.parallelStream().map { (qid, qtext) ->
+                    val qj = qrels[qid] ?: error("No judgments for $qid")
+                    val terms = index.tokenize(qtext)
+                    val qsdm = SequentialDependenceModel(terms, stopwords = inqueryStop)
+                    //val ql = QueryLikelihood(terms)
 
-                val results = index.search(ql, 100)
-                val gres = results.toQueryResults(index)
+                    val results = index.search(qsdm, 100)
 
-                val computed = measures.mapValues { (_, fn) -> fn.evaluate(gres, qj) }
-
-                synchronized(summary) {
-                    computed.forEach { m, score -> summary.push(m, score) }
-
-                    msg.incr()?.let { upd ->
-                        println(summary)
-                        println(upd)
+                    val rawDocs = results.scoreDocs.associate { sdoc ->
+                        val ldoc = index.document(sdoc.doc)!!
+                        val fields = pmake {}
+                        ldoc.fields.forEach { field ->
+                            val name = field.name()!!
+                            fields.putIfNotNull(name, field.stringValue())
+                            fields.putIfNotNull(name, field.numericValue())
+                        }
+                        Pair(sdoc.doc, fields)
                     }
-                }
+                    val docPs = results.scoreDocs.mapIndexed { i, sdoc ->
+                        val fields = rawDocs[sdoc.doc]!!
+                        val docName = fields.getString(index.idFieldName)!!
+                        pmake {
+                            set("id", docName)
+                            set("pooling-score", sdoc.score)
+                            set("rank", i + 1)
+                            set("fields", fields)
+                        }
+                    }
 
+                    val qjson = pmake {
+                        set("qid", qid)
+                        set("totalHits", results.totalHits)
+                        set("docs", docPs)
+                        set("qtext", qtext)
+                        set("qterms", terms)
+                    }
+
+                    val gres = results.toQueryResults(index)
+
+                    val computed = measures.mapValues { (_, fn) ->
+                        try {
+                            fn.evaluate(gres, qj)
+                        } catch (e: Exception) {
+                            println("Exception in eval: $e")
+                            0.0
+                        }
+                    }
+
+                    qjson.put("measures", Parameters.wrap(computed))
+
+                    synchronized(summary) {
+                        computed.forEach { m, score -> summary.push(m, score) }
+                        msg.incr()?.let { upd ->
+                            println(summary)
+                            println(upd)
+                        }
+                    }
+
+                    qjson
+                }.sequential().forEach { qjson ->
+                    output.println(qjson)
+                }
             }
         }
 
+        println("FINISHED.")
+        println(summary)
     }
 }
